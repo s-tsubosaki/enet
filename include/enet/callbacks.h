ef4fe7aa70bf68f0792e48789761822b2380251a
Shun Tsubosaki <s-tsubosaki@nbsi.jp>
2017-10-26 05:45:58 +0000

    未使用削除

--- include/enet/callbacks.h
+++ include/enet/callbacks.h
@@ -7,21 +7,13 @@
 
 #include <stdlib.h>
 
-typedef struct _ENetCallbacks
-{
-    void * (ENET_CALLBACK * malloc) (size_t size);
-    void (ENET_CALLBACK * free) (void * memory);
-    void (ENET_CALLBACK * no_memory) (void);
-} ENetCallbacks;
-
 /** @defgroup callbacks ENet internal callbacks
     @{
     @ingroup private
 */
-extern void * enet_malloc (size_t);
-extern void   enet_free (void *);
+extern void *enet_malloc(size_t);
+extern void enet_free(void *);
 
 /** @} */
 
 #endif /* __ENET_CALLBACKS_H__ */
-
